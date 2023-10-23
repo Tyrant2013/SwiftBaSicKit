3669306735b6a18a72fbd6eef2f69228160ace78
Zhuang Xiaowei <zhuangxiaowei_dev@163.com>
2023-10-23 13:28:22 +0800

    修复提示资源文件提示找不到的警告

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftBaSicKit", resources: [.process("FrameworkResources"), .process("Localizable")]),
+            name: "SwiftBaSicKit", resources: [.process("FrameworkResources.xcassets"), .process("Localizable.xcstrings")]),
         .testTarget(
             name: "SwiftBaSicKitTests",
             dependencies: ["SwiftBaSicKit"]),
